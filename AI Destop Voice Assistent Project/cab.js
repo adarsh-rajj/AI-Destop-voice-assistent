2bc5167dcc11a8b7a41a198ad40d891d94e9128a
Adarsh Raj <114833413+adarsh-rajj@users.noreply.github.com>
2023-04-14 16:28:40 +0530

    Add files via upload

---
+++ AI Destop Voice Assistent Project/cab.js
@@ -0,0 +1,9 @@
+let cab1 = prompt("enter price")
+let cab2 = prompt("second auto price")
+if(cab1 > cab2){
+    console.log("cab2")
+}else if(cab1<cab2){
+   console.log("cab1")
+}else{
+  console.log("any cab")
+}
